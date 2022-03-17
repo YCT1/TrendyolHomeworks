46ba31c24810195c735a94885e9087040ca1bbda
Yekta Can Tursun <yektacan1999@gmail.com>
2022-03-17 20:49:23 +0300

    finished with torch

--- HW1/debug.py
+++ HW1/debug.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 
-from model import ModelBase, ModelRegulizedL2, ModelRegulizedL1, ThresholdBasedModel
+from model import ModelBase, ModelRegulizedL2, ModelRegulizedL1, ThresholdBasedModel, Model2
 
 cal_housing = fetch_california_housing() 
 X = pd.DataFrame(cal_housing.data, columns=cal_housing.feature_names) 
@@ -12,5 +12,5 @@ df = pd.DataFrame(
 X, y = df["MedInc"].to_numpy(), df["Price"].to_numpy()   
 
 
-myModel3 = ThresholdBasedModel(threshold=5, lam=10**-1)
+myModel3 = Model2()
 myModel3.fit(X,y)
--- HW1/model.py
+++ HW1/model.py
@@ -1,10 +1,9 @@
 # Yekta Can Tursun
 
 
-
 from turtle import shape
 import numpy as np
-
+import torch
 
 class ModelBase():
     def __init__(self) -> None:
@@ -24,7 +23,7 @@ class ModelBase():
         self.beta = np.random.random(2)
         np.random.seed(10)
         print("starting sgd")
-        for i in range(1000):
+        for i in range(50):
             y_pred: np.ndarray = self.beta[0] + self.beta[1] * x
 
 
@@ -116,3 +115,47 @@ class ThresholdBasedModel(ModelBase):
             pass
         return g[0,:].mean(), g[1,:].mean()
 
+
+
+# Torch based Model
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.b0 = torch.nn.Parameter(torch.randn(()))
+        self.b1 = torch.nn.Parameter(torch.randn(()))
+    
+    def forward(self, x):
+        return self.b0 + self.b1 * x
+
+
+class Model2():
+    def __init__(self,threshold) -> None:
+        self.model = Model()
+        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)
+        self.threshold = threshold
+        pass
+    
+    def loss(self, y, y_pred):
+        loss = ((y_pred-y)**2)
+        loss[loss > self.threshold ] = self.threshold 
+        return torch.mean(loss)
+
+    def fit(self, x, y):
+        pass
+        x = torch.from_numpy(x)
+        y = torch.from_numpy(y)
+        self.model.train()
+        with torch.autograd.set_detect_anomaly(True):
+            for i in range(100):
+                y_pred = self.model(x)
+
+                loss = self.loss(y,y_pred)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+    
+    def pred(self, y) -> np.array:
+        y = torch.from_numpy(y)
+        self.model.eval()
+        y_pred = self.model(y)
+        return y_pred.detach().numpy()
